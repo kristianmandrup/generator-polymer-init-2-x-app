335bc32b24febb4fe26ffa8076a1d01df42744d7
Tony Trinh <tony19@gmail.com>
2017-03-04 20:44:39 -0600

    SW precache webcomponents-lite.js

--- generators/starter-kit/templates/sw-precache-config.js
+++ generators/starter-kit/templates/sw-precache-config.js
@@ -11,7 +11,7 @@ module.exports = {
   staticFileGlobs: [
     'index.html',
     'manifest.json',
-    'bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    'bower_components/webcomponentsjs/webcomponents-lite.js'
   ],
   navigateFallback: 'index.html'
 };
